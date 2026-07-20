5848d66c496d957332981f30d37c3bce968f49e4
Thiago Alves Gomes <thiagoalvesgomes@MacBook-Air-de-Thiago.local>
2026-07-20 16:21:39 -0300

    feat: add OCI Queue worker and OKE deployment assets

---
+++ backend/src/oci-clients.js
@@ -0,0 +1,46 @@
+const common = require("oci-common");
+const queue = require("oci-queue");
+const objectstorage = require("oci-objectstorage");
+
+let clients;
+
+function required(name) {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`Variável de ambiente obrigatória ausente: ${name}`);
+  }
+
+  return value;
+}
+
+function getOciClients() {
+  if (clients) {
+    return clients;
+  }
+
+  const provider =
+    common.OkeWorkloadIdentityAuthenticationDetailsProvider.builder().build();
+
+  const queueClient = new queue.QueueClient({
+    authenticationDetailsProvider: provider,
+  });
+  queueClient.endpoint = required("OCI_QUEUE_MESSAGES_ENDPOINT");
+
+  const objectStorageClient = new objectstorage.ObjectStorageClient({
+    authenticationDetailsProvider: provider,
+  });
+  objectStorageClient.regionId = required("OCI_REGION");
+
+  clients = {
+    queueClient,
+    objectStorageClient,
+    queueId: required("OCI_QUEUE_ID"),
+    objectStorageNamespace: required("OCI_OBJECT_STORAGE_NAMESPACE"),
+    bucketName: required("OCI_OBJECT_STORAGE_BUCKET"),
+  };
+
+  return clients;
+}
+
+module.exports = { getOciClients };
